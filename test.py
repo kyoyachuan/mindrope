f9a31c71a18e9bcb261c1a21df3c56198e5d3a31
kyoyachuan <kyoyachuan0626@gmail.com>
2022-08-11 21:43:15 +0800

    feat: write gif in test.py

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import random
 
+import numpy as np
 import hydra
 from omegaconf import DictConfig
 import torch
+import imageio
 
 from mindrope.dataset import get_dataloader
 from mindrope.svg import SVGModel
@@ -21,7 +23,11 @@ def main(cfg: DictConfig) -> None:
     test_dataloader = get_dataloader(cfg.data, mode='test', num_workers=cfg.base.num_workers)
     model = SVGModel(cfg.model, batch_size=cfg.data.batch_size, load_model=True)
     evaluator = Evaluator(model, test_dataloader)
-    psnr = evaluator.evaluate(cfg.data.n_past, cfg.data.n_future)
+    psnr, seq = evaluator.evaluate(cfg.data.n_past, cfg.data.n_future, return_last_seq=True)
+    video = (seq[1].cpu().numpy() * 255.).astype(np.uint8)
+    video = [v.transpose(1, 2, 0) for v in video]
+    imageio.mimsave('/home/kyoyachuan/mindrope/demo_test.gif', video, fps=4)
+
     print(f"Best PSNR: {psnr}")
 
 
